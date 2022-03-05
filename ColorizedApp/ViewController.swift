5816b301e9918a6110fc3d3088cc2fcc28489ebf
Nazar <nazar383875@gmail.com>
2022-03-05 16:13:47 +0300

    cod polovina

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -10,13 +10,33 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var colorСhangingView: UIView!
-
+    
+    @IBOutlet weak var redLabelValue: UILabel!
+    @IBOutlet weak var greenLabelValue: UILabel!
+    @IBOutlet weak var blueLabelValue: UILabel!
+    
+    @IBOutlet weak var redSliderOutlet: UISlider!
+    @IBOutlet weak var greenSliderOutlet: UISlider!
+    @IBOutlet weak var blueSliderOutlet: UISlider!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        redLabelValue.text = String(round(redSliderOutlet.value * 10) / 10)
+        greenLabelValue.text = String(round(greenSliderOutlet.value * 10) / 10)
+        blueLabelValue.text = String(round(blueSliderOutlet.value * 10) / 10)
     }
-
+    
+    @IBAction func redSliderAction() {
+        redLabelValue.text = String(round(redSliderOutlet.value * 10) / 10)
+    }
+    @IBAction func greenSliderAction() {
+        greenLabelValue.text = String(round(greenSliderOutlet.value * 10) / 10)
+    }
+    @IBAction func blueSliderAction() {
+        blueLabelValue.text = String(round(blueSliderOutlet.value * 10) / 10)
+    }
+    
 
 }
 
